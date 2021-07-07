dc1220fafe8b4521cec485b13ba96b9dae6cb22a
vasanth-anywhere <87004044+vasanth-anywhere@users.noreply.github.com>
2021-07-07 10:09:59 +0530

    Update Main.java
    Added Employee data.

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 package com.company;
-
+//Example for creation and execution of Objects and Classes
 public class Main {
 
     public static void main(String[] args) {
@@ -16,5 +16,46 @@ public class Main {
         for(Employee employee : employeeList) {
             if(employee != null) employee.displayEmployeeDetails();
         }
+    }
+}
+
+public class Employee {
+    public int employeeID;
+    private String employeeName;
+    private int age;
+    private Address address;
+    public Employee(int employeeID, String employeeName, int age, int doorNumber,
+    String streetName, String landmark, String city, int pincode) {
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.age = age;
+        this.address = new Address(doorNumber, streetName, landmark, city, pincode);
+    }
+    public void displayEmployeeDetails() {
+        System.out.println("Employee Details : ");
+        System.out.println("Employee ID : "+employeeID);
+        System.out.println("Employee Name : "+employeeName);
+        System.out.println("Employee Age : "+age);
+        System.out.println("Employee address : "+this.address.display());
+        System.out.println();
+
+    }
+}
+
+public class Address {
+    private int doorNumber;
+    private String streetName;
+    private String landmark;
+    private String city;
+    private int pincode;
+    public Address(int doorNumber, String streetName, String landmark, String city, int pincode) {
+        this.doorNumber = doorNumber;
+        this.city = city;
+        this.landmark = landmark;
+        this.pincode = pincode;
+        this.streetName = streetName;
+    }
+    public String display() {
+        return (doorNumber+", "+streetName+", "+landmark+", "+city+", "+pincode);
     }
 }
